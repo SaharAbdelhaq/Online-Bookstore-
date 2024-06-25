8a4cd96e993a87f0d4298f09eedd11ac7b5db6e2
Sahar Abdelhaq <saharabdelhaq02@gmail.com>
2024-06-25 16:24:27 +0300

    User managment API is added

--- middlewares/isAuth.js
+++ middlewares/isAuth.js
@@ -1,20 +1,26 @@
 const jwt = require("jsonwebtoken");
 
-exports.isAuth = (req, res) => {
-  const token = req.header("Authorization")?.split(" ")[1];
-  if (!token) {
-    return res.status(401).json({
-      message: "Access denied",
-    });
-  }
+exports.isAuth = (req, res, next) => {
   try {
-    let decoded = jwt.verify(token, SaharSecretKey);
-    if (!decodedToken) {
+    if (!req.get("Authorization")) {
+      return res.status(401).json({
+        message: "not Authenticated",
+      });
+    }
+    const token = req.get("Authorization")?.split(" ")[1];
+    if (!token) {
+      return res.status(401).json({
+        message: "not Authenticated",
+      });
+    }
+    let decoded = jwt.verify(token, "SaharSecretKey");
+    if (!decoded) {
       return res.status(403).json({
         message: "Authentication failed",
       });
     }
     req.userId = decoded.userId;
+    next();
   } catch (e) {
     console.log(e);
   }
